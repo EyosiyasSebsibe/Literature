c0bbaf8cfba93f6287844ce78c4eba6a077095c3
Eyosiyas <eyosiyas7799@gmail.com>
2026-05-03 21:09:38 +0300

    finalized with the main class

---
+++ Main.java
@@ -0,0 +1,96 @@
+class Literature {
+    protected String title;
+
+    Literature(String title) {
+        this.title = title;
+    }
+
+    void display(){
+        System.out.println("Title: " + title);  
+    }
+
+    static void category(){
+        System.out.println("General Literature");
+    }
+}
+
+
+
+class Author extends Literature {
+    protected String name;
+
+    Author(String title, String name) {
+        super(title);
+        this.name = name;
+    }
+
+    @Override
+    void display() {
+        super.display();
+        System.out.println("Author: " + name);
+    }
+
+    static void category() {
+        System.out.println("Author's Literature");
+    }
+}
+
+
+
+class Shakespeare extends Author {
+    Shakespeare(String title){
+        super(title, "William Shakespeare");
+    }
+    @Override
+    void display(){
+        super.display();
+        System.out.println("Masterpiece");
+    }
+}
+    
+
+
+
+
+class Poet extends Literature {
+   Poet(String title) {
+    super(title);
+   }
+
+    @Override
+    void display() {
+        super.display();
+        System.out.println("Poem title: " + title);
+    }
+}
+
+
+final class English{
+    final void rules(){
+        System.out.println("Grammer rules are unchangeable");
+    }
+}
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Literature literature = new Literature("General Literature");
+        literature.display();
+        Literature.category();
+
+        Author author = new Author("Hamlet", "William Shakespeare");
+        author.display();
+        Author.category();
+
+        Shakespeare shakespeare = new Shakespeare("Romeo and Juliet");
+        shakespeare.display();
+
+        Poet poet = new Poet("The Raven");
+        poet.display();
+
+        English english = new English();
+        english.rules();
+    }
+}
