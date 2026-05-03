56621ba36d890a434fe6204ddd0d2124d3789a9e
Eyosiyas <eyosiyas7799@gmail.com>
2026-05-03 20:39:26 +0300

    created author class

--- Literature.java
+++ Literature.java
@@ -9,9 +9,30 @@ class Literature {
     void display(){
         System.out.println("Title: " + title);  
     }
-    
+
     static void category(){
         System.out.println("General Literature");
+    }
+}
+
+
+
+class Author extends Literature {
+    protected String name;
+
+    Author(String title, String name) {
+        super(title);
+        this.name = name;
+    }
+
+    @Override
+    void display() {
+        super.display();
+        System.out.println("Author: " + name);
+    }
+
+    static void category() {
+        System.out.println("Author's Literature");
     }
 }
     
